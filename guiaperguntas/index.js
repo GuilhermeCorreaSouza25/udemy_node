ab0e5fd662f98948a362a3f82f3acce29d4c7cb9
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-08-19 09:29:44 -0300

    feat
    bootstrap

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -4,28 +4,36 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
+app.get('/',(req,res) =>{
+  res.render("index");
+})
 
-  var produtos = [
-    {nome: "Doritos", preco: 3.14},
-    {nome: "Coca-cola", preco: 5},
-    {nome: "Leite", preco: 2.45},
-    {nome: "Carne", preco: 25},
-    {nome: "Arroz", preco: 10},
-  ]
+app.get('/perguntar', (req,res)=>{
+  res.render("perguntar");
+})
 
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "Soter Tec",
-    inscritos: 10,
-    msg: exibirMsg,
-    produtos: produtos
-  });
-});
+// app.get("/:nome/:lang", (req, res) => {
+//   var nome = req.params.nome;
+//   var lang = req.params.lang;
+//   var exibirMsg = false;
+
+//   var produtos = [
+//     {nome: "Doritos", preco: 3.14},
+//     {nome: "Coca-cola", preco: 5},
+//     {nome: "Leite", preco: 2.45},
+//     {nome: "Carne", preco: 25},
+//     {nome: "Arroz", preco: 10},
+//   ]
+
+//   res.render("index", {
+//     nome: nome,
+//     lang: lang,
+//     empresa: "Soter Tec",
+//     inscritos: 10,
+//     msg: exibirMsg,
+//     produtos: produtos
+//   });
+// });
 
 app.listen(8080, () =>{
   console.log("App rodando!");
