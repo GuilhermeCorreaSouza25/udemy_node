811a083a0f256073176b3872008eb9f466ec815c
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-08-22 20:22:33 -0300

    feat
    update

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,61 +1,88 @@
 const express = require("express");
 const app = express();
-const bodyParser =  require("body-parser")
-const connection = require('./database/database')
-
-//database
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
+//Database
 connection
-  .authenticate()
-  .then(() => {
-    console.log("Conexão feita com o banco de dados!")
-  })
-  .catch((msgErro) => {
-    console.log(msgErro);
-  }
-);
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
 
-app.set('view engine', 'ejs');
+// Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine','ejs');
 app.use(express.static('public'));
-
+// Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/',(req,res) =>{
-  res.render("index");
-})
-
-app.get('/perguntar', (req,res)=>{
-  res.render("perguntar");
-})
-
-app.post('/salvarpergunta', (req, res)=>{
-  var titulo = req.body.titulo;
-  var descricao = req.body.descricao;
-  res.send("Formulário recebido!")
-})
-// app.get("/:nome/:lang", (req, res) => {
-//   var nome = req.params.nome;
-//   var lang = req.params.lang;
-//   var exibirMsg = false;
-
-//   var produtos = [
-//     {nome: "Doritos", preco: 3.14},
-//     {nome: "Coca-cola", preco: 5},
-//     {nome: "Leite", preco: 2.45},
-//     {nome: "Carne", preco: 25},
-//     {nome: "Arroz", preco: 10},
-//   ]
-
-//   res.render("index", {
-//     nome: nome,
-//     lang: lang,
-//     empresa: "Soter Tec",
-//     inscritos: 10,
-//     msg: exibirMsg,
-//     produtos: produtos
-//   });
-// });
-
-app.listen(8080, () =>{
-  console.log("App rodando!");
+// Rotas
+app.get("/",(req, res) => {
+    Pergunta.findAll({ raw: true, order:[
+        ['id','DESC'] // ASC = Crescente || DESC = Decrescente
+    ]}).then(perguntas => {
+        res.render("index",{
+            perguntas: perguntas
+        });
+    });
 });
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+})
+
+app.post("/salvarpergunta",(req, res) => {
+
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
+});
+
+app.get("/pergunta/:id",(req ,res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined){ // Pergunta encontrada
+
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order:[ 
+                    ['id','DESC'] 
+                ]
+            }).then(respostas => {
+                res.render("pergunta",{
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+
+        }else{ // Não encontrada
+            res.redirect("/");
+        }
+    });
+})
+
+app.post("/responder",(req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId);
+    });
+});
+
+app.listen(8080,()=>{console.log("App rodando!");})
