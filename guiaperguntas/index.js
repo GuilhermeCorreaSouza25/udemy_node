e9168de9f4df00c5f6c3096657fcc5f8b63230f3
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-08-22 15:13:02 -0300

    feat
    update

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser =  require("body-parser")
+const connection = require('./database/database')
+
+//database
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com o banco de dados!")
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
+  }
+);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/',(req,res) =>{
   res.render("index");
@@ -12,6 +28,11 @@ app.get('/perguntar', (req,res)=>{
   res.render("perguntar");
 })
 
+app.post('/salvarpergunta', (req, res)=>{
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("Formulário recebido!")
+})
 // app.get("/:nome/:lang", (req, res) => {
 //   var nome = req.params.nome;
 //   var lang = req.params.lang;
