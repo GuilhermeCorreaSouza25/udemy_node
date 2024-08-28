0a3975acd8fbaf38680b52c2f5fb57299ed0f0d6
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-08-28 13:48:55 -0300

    feat: update

--- GUIAPRESS/articles/articlesController.js
+++ GUIAPRESS/articles/articlesController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/category')
+const slugify = require('slugify')
+const Article = require('./article')
 
 router.get('/articles', (req, res) => {
     res.send('ROTA DE ARTIGOS');
@@ -10,6 +12,21 @@ router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories});
     })
+})
+
+router.post('/articles/save', (req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var categoryId = req.body.category;
+    if(title != undefined && body != undefined){
+        Article.create({
+            title: title,
+            slug: slugify(title),
+            body: body,
+            categoryId: categoryId
+        }).then(() => {
+            res.redirect('/admin/articles');
+        })}
 })
 
 module.exports = router;
