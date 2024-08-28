52183c6ba9682628b73737d0de756e5aeb29940f
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-08-28 17:45:41 -0300

    feat: update

--- GUIAPRESS/index.js
+++ GUIAPRESS/index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const session = require("express-session");
 const connection = require('./database/database')
 
 // Controllers
 const categoriesController = require('./categories/CategoriesController');
-const articlesController = require('./articles/articlesController');
+const articlesController = require('./articles/ArticlesController');
+const usersController = require("./users/UsersController");
 
 // Models
-const Article = require('./articles/article');
-const Category = require('./categories/category');
-
+const Article = require('./articles/Article');
+const Category = require('./categories/Category');
+const User = require("./users/User");
 
 // View engine
 app.set('view engine', 'ejs');
+
+// Sessions
+app.use(session({
+    secret: "qualquercoisa", cookie: { maxAge: 30000000 }
+}))
 
 // Static Files
 app.use(express.static('public'));
@@ -36,11 +43,61 @@ connection
 // Routes
 app.use('/', categoriesController);
 app.use('/', articlesController);
+app.use("/", usersController);
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ],
+        limit: 4
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });
+    });
 })
 
-app.listen(8080, ()=>{
-    console.log('Servidor rodando !!');
+app.get("/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    });
+})
+
+app.get("/category/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index",{articles: category.articles,categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log("O servidor está rodando!")
 })
