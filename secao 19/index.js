074dc451289b9b643c32c84801cd37cef3792a22
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-09-13 18:19:05 -0300

    feat: update

--- secao 19/index.js
+++ secao 19/index.js
@@ -1,13 +1,53 @@
 var database = require("./database");
 
-var dados = {
-    nome: "Sea of thieves",
-    preco: 100.00
-}
+// var dados = [
+//     {
+//         nome: "Call of Duty",
+//         preco: 120.00
+//     },
+//     {
+//         nome: "Call of Duty 2",
+//         preco: 245.00
+//     },
+//     {
+//         nome: "GTA V",
+//         preco: 32.68
+//     }
+// ]
 
-var query = database.insert(dados).into("games").then(data =>{
+// INSERT
+// var query = database.insert(dados).into("games").then(data =>{
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// SELECT
+// database.select(["id", "preco"]).table("games").then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// })
+
+// WHERE
+// database.select()
+//         .whereRaw("nome = 'GTA V' OR preco > 120")
+//         .table("games").then(data => {
+//             console.log(data);
+//         }).catch(err => {
+//             console.log(err);
+//         });
+
+// DELETE
+// database.where({id: 3}).delete().table("games").then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// UPDATE
+database.where({id: 1}).update({preco: 100}).table("games").then(data => {
     console.log(data);
 }).catch(err => {
     console.log(err);
 });
-
