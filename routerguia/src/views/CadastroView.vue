67275494ea0b75bff6b80e0934f9fa3b510acb19
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-09-17 18:27:26 -0300

    feat:
    update

---
+++ routerguia/src/views/CadastroView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="text" name="email" id="email" placeholder="E-mail" /><br />
+        <input type="text" name="nome" id="nome" placeholder="Nome" /><br />
+        <button>Teste!</button>
+        <h4>{{ parametroDaRota }}</h4>
+    </div>
+</template>
+<script>
+export default {
+    created(){
+        this.parametroDaRota = this.$route.params.dado;
+    },
+    data(){
+        return {
+            parametroDaRota: 'Nada'
+        }
+    }
+}
+</script>
