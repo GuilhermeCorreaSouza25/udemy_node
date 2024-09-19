24e6175bc4a21cea3a3a33b02c83bbdffaa94396
Guilherme Correa Souza <guilhermecorreasouza@gmail.com>
2024-09-19 14:50:47 -0300

    feat:
    update projeto 6

--- secao 24/users/src/App.vue
+++ secao 24/users/src/App.vue
@@ -10,19 +10,19 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
-#nav {
+nav {
   padding: 30px;
 }
 
-#nav a {
+nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
--- secao 24/users/src/router/index.js
+++ secao 24/users/src/router/index.js
@@ -1,78 +1,30 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Register from '../views/Register.vue'
-import Login from '../views/Login.vue'
-import Users from '../views/Users.vue'
-import axios from 'axios';
-import Edit from '../views/Edit.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Register from '../views/RegisterView.vue'
 
-
-function AdminAuth(to, from, next){
-  if(localStorage.getItem('token') != undefined){
-
-    var req = {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem('token')
-      }
-    }
-
-    axios.post("http://localhost:8686/validate",{},req).then(res => {
-      console.log(res);
-      next();
-    }).catch(err => {
-      console.log(err.response);
-      next("/login");
-    });
-  }else{
-    next("/login");
-  }
-}
-
-
-Vue.use(VueRouter)
-
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: HomeView
   },
   {
     path: '/register',
-    name: 'Register',
+    name: 'register',
     component: Register
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/admin/users',
-    name: 'Users',
-    component: Users,
-    beforeEnter: AdminAuth
-  },
-  {
-    path: '/admin/users/edit/:id',
-    name: 'UserEdit',
-    component: Edit,
-    beforeEnter: AdminAuth
-  },
-  {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
